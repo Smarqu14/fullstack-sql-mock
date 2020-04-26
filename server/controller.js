2744601784b89356498fb1bf6dc9584962ba341a
Michael Siu <michaeltsiu13@gmail.com>
2020-04-26 10:21:18 -0700

    GET and POST are working for controller.js

--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,27 @@
 // Controller here
 // complete building out the controller
-
+const helpers = require('../db/dbhelpers.js')
 
 const controller = {
-  get: (req, res) => {},
-  post: (req, res) => {},
-  put: (req, res) => {},
-  delete: (req, res) => {}
+  get: (req, res) => {
+    helpers.getProductsHelper((err, results) => {
+      if (err) { res.status(404).send(err) }
+      res.status(200).json(results);
+    })
+  },
+  post: (req, res) => {
+    let product = req.body
+    helpers.postProductsHelper(product, (err, results) => {
+      if (err) { res.status(404).send(err) }
+      res.status(200).send(results);
+    })
+  },
+  put: (req, res) => {
+
+  },
+  delete: (req, res) => {
+
+  }
 }
 
 module.exports = controller
