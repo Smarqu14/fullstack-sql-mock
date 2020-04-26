eef546d852e189c26e5e65ee85ac13f3300fa652
Michael Siu <michaeltsiu13@gmail.com>
2020-04-26 10:18:33 -0700

    Schema has been set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,12 @@
+DROP DATABASE IF EXISTS mockdata;
+CREATE DATABASE mockdata;
+USE mockdata;
 
+CREATE TABLE products (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  item VARCHAR(255) NOT NULL,
+  min_cost FLOAT(24) NOT NULL,
+  curr_bid FLOAT(24) NOT NULL,
+  ends_in INT NOT NULL,
+  image VARCHAR(255)
+);
