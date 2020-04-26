d0f207c924f49497b3bbedfdbc0876f2fd24abf5
Michael Siu <michaeltsiu13@gmail.com>
2020-04-26 10:20:24 -0700

    GET and POST are working for db.helpers.js

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,11 +1,26 @@
 // complete and fix the dbhelpers
+const db = require('./index.js')
 
+const helpers = {
+  getProductsHelper: (callback) => {
+    let queryStr = `SELECT * FROM products;`;
+    db.query(queryStr, (err, results) => {
+      callback(err, results)
+    })
+  },
+  postProductsHelper: (product, callback) => {
+    let { item, min_cost, curr_bid, ends_in, image } = product;
+    let queryStr = `INSERT INTO products (item, min_cost, curr_bid, ends_in, image) VALUES ('${item}', ${min_cost}, ${curr_bid}, ${ends_in}, '${image}');`;
+    db.query(queryStr, (err, results) => {
+      callback(err, results);
+    })
+  },
+  updateProductHelper: (callback) => {
 
-getProductsHelper = () => 
+  },
+  deleteProductHelper: (callback) => {
 
-postProductsHelper = () => 
+  }
+}
 
-updateProductHelper = () => 
-
-deleteProductHelper = () => 
-
+module.exports = helpers;
